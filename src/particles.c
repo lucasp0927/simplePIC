e6f1067c752f01ce36e85cb3052cb44398bdd6bb
Lucas Peng <lucasp0927@gmail.com>
2012-09-17 13:23:45 +0800

    add species

--- src/particles.c
+++ src/particles.c
@@ -1,6 +1,7 @@
 #include "particles.h"
 int particles_init(Particles* P, int s_num, int p_num, int d)
 {
+
   int ret = 0;//return value
   P->species_num = s_num;
   P->particle_num = p_num;
@@ -10,6 +11,9 @@ int particles_init(Particles* P, int s_num, int p_num, int d)
   P->vel = (double**) calloc (s_num,sizeof(double*));
   P->pos_1 = (double**) calloc (s_num,sizeof(double*));
   P->vel_1 = (double**) calloc (s_num,sizeof(double*));
+
+  /* initialize species */
+  P->species = (Species*) malloc(s_num*sizeof(Species));
   
   double** tmp [] = {P->pos,P->vel,P->pos_1,P->vel_1};
   int i,j;
@@ -41,19 +45,35 @@ int particles_init(Particles* P, int s_num, int p_num, int d)
 
 int particles_print(Particles* P)
 {
-  printf ("%s\n","Print out all particles");
+  printf ("%s\n","--------------------");      
   int i,j;
   for (i=0;i<P->species_num;i++)
   {
+    printf ("%s %d\n","species ",i);
+    species_print(&(P->species[i]));
+    printf ("%s\n","--------------------");    
+  }
+  
+  printf ("%s\n","Print out all particles");
+
+  for (i=0;i<P->species_num;i++)
+  {
     printf ("%s %d\n","species",i);
-    printf ("%s\n","----------------------------------------------------");
     if (P->dim == 3)
       for (j=0;j<P->particle_num;j++)
         printf ("%5d: p:%10e %10e %10e v:%10e %10e %10e \n",j,P->pos[i][3*j],P->pos[i][3*j+1],P->pos[i][3*j+2],P->vel[i][3*j],P->vel[i][3*j+1],P->vel[i][3*j+2]);
     if (P->dim == 2)
       for (j=0;j<P->particle_num;j++)
-        printf ("%5d: p:%10e %10e v:%10e %10e \n",j,P->pos[i][2*j],P->pos[i][2*j+1],P->vel[i][2*j],P->vel[i][2*j+1]);    
+        printf ("%5d: p:%10e %10e v:%10e %10e \n",j,P->pos[i][2*j],P->pos[i][2*j+1],P->vel[i][2*j],P->vel[i][2*j+1]);
+    printf ("%s\n","------------------------------------------------------------------------");    
   }
   
+  return 0;
+}
+
+extern int species_print(Species* S)
+{
+  printf ("%s %f\n","mass:",S->mass);
+  printf ("%s %f\n","charge:",S->charge);
   return 0;
 }
--- src/particles.h
+++ src/particles.h
@@ -2,12 +2,20 @@
 #define PARTICLES_H
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct
+{
+  double mass;
+  double charge;
+} Species;
+
+
 typedef struct 
 {
   double** pos;
   double** vel;
   double** pos_1;
   double** vel_1;
+  Species* species;
   int species_num;
   int particle_num;
   int dim;
@@ -20,5 +28,6 @@ extern int particles_init(Particles* P, int s_num, int p_num, int d);
   d: dimension
 */
 extern int particles_print(Particles* P);
+extern int species_print(Species* S);
 #endif
 
